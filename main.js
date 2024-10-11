1bcf3710b5253396dc95a359ee1bea759c877db7
RaulAM <raulartilesm@gmail.com>
2024-10-11 12:31:20 +0100

    Update: data inmutability object assign YES and spread operator NO

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const source1 = { b: 2 }
 const source2 = { c: 3, d:4 }
 
 
-Object.assign(target, source1, source2)
+Object.assign(target, source1, source2) //SINTAXIS BASICA DEL OBJECT ASSIGN
 
 console.log(target)
 
@@ -58,3 +58,23 @@ const updateLocation = (sourceObj, newLocation) => {
 const newSchool = updateLocation(school, 'Galdar')
 
 console.log(newSchool)
+newSchool.location = 'La isleta'
+console.log(newSchool)
+console.log(school)
+
+
+
+// SPREAD OPERATOR
+// Se puede usar el Spread Operator para copiar objects?
+// Si que se puede usar preo estan REFERENCIADOS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const Object1 = { a: 0, b: 1, c: { tio: 'pepe' } }
+const Object2 = { d: 2, e: 4, e: { tia: 'mari' } }
+
+const pruebaSpreadOperator = {...Object1, ...Object2}
+console.log(pruebaSpreadOperator)
+
+pruebaSpreadOperator.b = 'Si que está referenciado con el Spread Operator'
+console.log(pruebaSpreadOperator)
+
+
+// 
